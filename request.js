f605a4b6215d2314ef1e07bd92b1296f0d3c6a66
Alemph <jlsangregorio@up.edu.ph>
2024-03-07 15:32:52 +0800

    chore:added needle.get for query

--- request.js
+++ request.js
@@ -2,11 +2,21 @@ import needle from 'needle';
 
 // Example data for testing the POST method
 const bookData = {
-  Bookname: "Harry Potter and the Philosopher’s Stone",
-  ISBN: "978-0-7475-3269-9",
+  Bookname: "Harry Potter and the Chamber of Secrets",
+  ISBN: "0-7475-3849-2",
   Author: "J.K Rowling",
-  Year_Published: "1997"
+  Year_Published: "1998"
 };
+
+needle.get('http://localhost:3000/find-by-author?author=J.K+Rowling', (err, res) => {
+  if (err) throw err;
+  console.log('Books by J.K Rowling:', res.body);
+});
+
+needle.get('http://localhost:3000/find-by-isbn-author?isbn=0-7475-3849-2&author=J.K+Rowling', (err, res) => {
+  if (err) throw err;
+  console.log('Book found:', res.body);
+});
 
 needle.post('http://localhost:3000/add-book', bookData, (err, res) => {
   if (err) throw err;
